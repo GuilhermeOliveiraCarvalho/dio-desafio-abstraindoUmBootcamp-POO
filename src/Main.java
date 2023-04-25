65770c6d0bfa0a3761361e2c128ec5a31b3feede
Guilherme <gui17oliveira@gmail.com>
2023-04-25 18:44:35 -0300

    Adicionando cursos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import br.com.dio.desafio.abstraindoBootcamp.Curso;
 
 public class Main {
     public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Introducao a Linguagem Java");
+        curso1.setDescricao("Curso feito para iniciantes em Java");
+        curso1.setCargaHoraria(12);
+
+        Curso curso2 = new Curso ();
+        curso2.setTitulo("Introducao ao Kotlin");
+        curso2.setDescricao("Curso feito para iniciantes em Kotlin");
+        curso2.setCargaHoraria(9);
+
 
         }
     }
